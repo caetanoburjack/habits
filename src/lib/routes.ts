f49ab9a44b39d52a073893ce44855202270820c6
Caetano Burjack <caetano.burjack@gmail.com>
2023-01-22 10:58:13 +0000

    Atualizando rotas no prisma

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -70,4 +70,63 @@ export async function appRoutes(app: FastifyInstance) {
             completedHabits
         }
     })
+
+    app.patch('/habits/:id/toggle', async (request) => {
+        const toggleHabitParams = z.object({
+            id: z.string().uuid(),
+        })
+
+        const { id } = toggleHabitParams.parse(request.params)
+        const today = dayjs().startOf('day').toDate()
+
+        let day = await prisma.day.findUnique({
+            where: {
+                date: today,
+            }
+        })
+
+        if (!day) {
+            day = await prisma.day.create({
+                data: {
+                    date: today,
+                }
+            })
+        }
+        const dayHabit = await prisma.dayHabit.findUnique({
+            where: {
+                day_id_habit_id: {
+                    day_id: day.id,
+                    habit_id: id,
+                }
+            }
+        })
+
+        if (dayHabit) {
+            await prisma.dayHabit.delete({
+                where: {
+                    id: dayHabit.id,
+                }
+            })
+        } else {
+            await prisma.dayHabit.create({
+                data: {
+                    day_id: day.id,
+                    habit_id: id,
+                }
+            })
+        }
+        return (dayHabit)
+    })
+
+    app.get('/summary', async () => {
+        const summary = await prisma.$queryRaw`
+            select d.id, d.date,
+            (select cast(count(*) as float) from day_habits dh where dh.day_id = d.id) as completed,
+            (select cast(count(*) as float) from habit_week_days hwd join habits h where hwd.week_day = cast(strftime('%w', d.date/1000.0, 'unixepoch') as int)) as amount
+             from days d
+            `
+
+        return summary
+    })
+
 }
